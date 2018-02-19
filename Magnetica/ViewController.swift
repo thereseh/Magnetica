f1880f3de2f4db0d170d1bdaca2bb0604a1573de
Therese Henriksson (RIT Student) <tah5279@twctechimac02.main.ad.rit.edu>
2018-02-19 12:26:46 -0500

    Implemented info response on table theme click

--- Magnetica/ViewController.swift
+++ Magnetica/ViewController.swift
@@ -32,60 +32,60 @@ class ViewController: UIViewController {
         var row: CGFloat = 0
         
         for word in words {
-            let l = UILabel()
-            l.backgroundColor = UIColor.white
-            l.text = word
-            l.sizeToFit()
+            let currentLabel = UILabel()
+            currentLabel.backgroundColor = UIColor.white
+            currentLabel.text = word
+            currentLabel.sizeToFit()
             
             // resize the labels
-            l.frame.size.height = 30
-            l.frame.size.width = l.frame.size.width + 15
+            currentLabel.frame.size.height = 30
+            currentLabel.frame.size.width = currentLabel.frame.size.width + 15
             
             var x: CGFloat = 0
             var y: CGFloat = 0
             
             if count == 0 {
                 
-                x = margin + ( l.frame.size.width / 2 )
+                x = margin + ( currentLabel.frame.size.width / 2 )
                 y = margin
                 
             } else {
                 // (previous X position) + (half width of previous word) + (half width of current word)
                 // would place each word next to each other, then add 10 units of space
-                let test: CGFloat = ( ( prevWord[0] + ( prevWord[1] / 2 ) ) + ( l.frame.size.width / 2 ) ) + 15
+                let nextSize: CGFloat = ( ( prevWord[0] + ( prevWord[1] / 2 ) ) + ( currentLabel.frame.size.width / 2 ) ) + 15
                 
                 // if it's more than width, then wrap around again
-                if test > CGFloat( width ) {
+                if nextSize > CGFloat( width ) {
                     
-                    x = margin + ( l.frame.size.width / 2 )
+                    x = margin + ( currentLabel.frame.size.width / 2 )
                     row = row + 1.0
                     
                 } else {
-                    x = test
+                    x = nextSize
                 }
 
-                y = margin + ( l.frame.size.height * row ) + ( 15 * row )
+                y = margin + ( currentLabel.frame.size.height * row ) + ( 15 * row )
         
             }
             
             // add current word into array for next word to use
             prevWord[0] = x
-            prevWord[1] = l.frame.size.width
+            prevWord[1] = currentLabel.frame.size.width
 
-            l.center = CGPoint( x:x, y:y )
-            l.textAlignment = .center
-            view.addSubview(l)
+            currentLabel.center = CGPoint( x:x, y:y )
+            currentLabel.textAlignment = .center
+            view.addSubview(currentLabel)
             
             // add dropshadow
-            l.layer.shadowColor = UIColor.black.cgColor
-            l.layer.shadowRadius = 1.0
-            l.layer.shadowOpacity = 0.8
-            l.layer.shadowOffset = CGSize(width:1, height: 1)
-            l.layer.masksToBounds = false
+            currentLabel.layer.shadowColor = UIColor.black.cgColor
+            currentLabel.layer.shadowRadius = 1.0
+            currentLabel.layer.shadowOpacity = 0.8
+            currentLabel.layer.shadowOffset = CGSize(width:1, height: 1)
+            currentLabel.layer.masksToBounds = false
             
-            l.isUserInteractionEnabled = true
+            currentLabel.isUserInteractionEnabled = true
             let panGesture = UIPanGestureRecognizer(target: self, action: #selector(doPanGesture))
-            l.addGestureRecognizer(panGesture)
+            currentLabel.addGestureRecognizer(panGesture)
             count += 1
         }
     }
@@ -94,6 +94,13 @@ class ViewController: UIViewController {
         let label = panGeture.view as! UILabel
         let position = panGeture.location(in: view)
         label.center = position
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showThemeSegue" {
+            let wordsVC = segue.destination.childViewControllers[0] as! WordsTableVC
+            wordsVC.title = "Choose a theme"
+        }
     }
 
 
--- Magnetica/WordsTableVC.swift
+++ Magnetica/WordsTableVC.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class WordsTableVC: UITableViewController {
 
+    let word1:[String] = ["could","cloud","bot","bit","ask","a","geek","flame","file","ed","ed","create","like","lap","is","ing","I","her","drive","get","soft","screen","protect","online","meme","to","they","that","tech","space","source","y","write","while"]
+    let word2 = ["she", "sinister", "smart", "so", "soft", "some", "source", "space"]
+    let word3 = ["good", "goodness", "grease", "has", "have", "he", "hear", "heart", "her"]
+    
+    var wordThemes:[(name: String, value: [String])] = []
+    
+    var selectedWordTheme:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        wordThemes.append((name: "Theme1", value: word1))
+        wordThemes.append(contentsOf: [(name: "Theme2", value: word2)])
+        wordThemes += [(name: "Theme3", value: word3)]
+        
+        selectedWordTheme = word1
     }
 
 
@@ -24,7 +37,7 @@ class WordsTableVC: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 3
+        return wordThemes.count
     }
 
     
@@ -32,52 +45,23 @@ class WordsTableVC: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
+        let currentTuple = wordThemes[indexPath.row]
+        
+        let currentTupleName = currentTuple.name
+        
+        cell.textLabel?.text = currentTupleName
+        
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedWordTheme = wordThemes[indexPath.row].value
+        print(selectedWordTheme)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @IBAction func cancelTapped(sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
